0f91b98cb06ffa819547ebb854093636098bc56e
elaheh <elahehazari9@gmail.com>
2020-07-20 22:03:47 -0700

    first commit

--- doz.js
+++ doz.js
@@ -1,7 +1,22 @@
 
+var turn = 1;
 function xo(id) {
-    var turn = 1;
-    if
-        document.getElementById(id).innerHTML = ;
+    if (document.getElementById(id).innerHTML == "&nbsp;")
+        if ((turn % 2) != 0) {
+            document.getElementById(id).innerHTML = "x"
+            winner();
+            turn++;
+        }
+        else {
+            document.getElementById(id).innerHTML = "o"
+            winner();
+            turn++;
+        }
+
+    function winner() {
+        if (turn > 4) {
+            if (document.getElementById(11).innerHTML ==)
+          }
+    }
 
 }
